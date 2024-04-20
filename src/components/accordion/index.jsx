9c99c260ec9e675c729f05d40bb5fdb3b8c6d555
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-04-20 15:32:52 +0530

    login for get previes value ,filter it .. add in array..

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -6,19 +6,35 @@ export default function Accordion() {
   const [multiSelected, setMultiSelected] = useState(false);
   const [selected, setSelected] = useState(null);
   const [multiselect, setMultiselect] = useState([]);
+  console.log(selected);
+
   const accordionToggleHandler = (id) => {
+    console.log(id);
     setSelected(id === selected ? null : id);
   };
-  const handleMultiSelection = (id) => {
-    setSelected((prev) =>
-      prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]
-    );
-  };
+  // const handleMultiSelection = (getCurrentId) => {
+  //   console.log(getCurrentId);
+  //   let cpyMutiple = [...multiselect];
+  //   const findIndexOfCurrentId = cpyMutiple.indexOf(getCurrentId);
 
+  //   console.log(findIndexOfCurrentId);
+  //   if (findIndexOfCurrentId === -1) cpyMutiple.push(getCurrentId);
+  //   else cpyMutiple.splice(findIndexOfCurrentId, 1);
+  //   setMultiselect(cpyMutiple);
+  // };
+  const handleMultiSelection = (getCurrentId) => {
+    setMultiselect((prev) => {
+      if (prev.includes(getCurrentId)) {
+        return prev.filter((id) => id !== getCurrentId);
+      } else {
+        return [...prev, getCurrentId];
+      }
+    });
+  };
   return (
     <div className="wrapper">
       <button onClick={() => setMultiSelected(!multiSelected)}>
-        Enable Multi Selection
+        {multiSelected ? "Disable Multi Selection" : "Enable Multi Selection"}
       </button>
       <div className="accordian">
         {data && data.length > 0 ? (
@@ -35,11 +51,13 @@ export default function Accordion() {
                 <h3> {item.question}</h3>
                 <span>+</span>
               </div>
-              {selected === item.id && (
-                <div className="content">
-                  <p>{item.answer}</p>
-                </div>
-              )}
+              {multiSelected
+                ? multiselect.indexOf(item.id) !== -1 && (
+                    <div className="acc-content ">{item.answer}</div>
+                  )
+                : selected === item.id && (
+                    <div className="acc-content ">{item.answer}</div>
+                  )}
             </div>
           ))
         ) : (
